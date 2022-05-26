5ce7dbf33b4b7a2034270e772b1ccb9f3b939ba9
Deniz Caglar <denizcaglar200@gmail.com>
2022-05-26 14:03:50 +0300

    removed demo files

--- backend/demo.py
+++
@@ -1,8 +0,0 @@
-from mediaplay import MediaPlay
-import pdb
-
-pdb.set_trace()
-med = MediaPlay()
-cookie_dict = med.parse_cookies_from_file(cookieFile="../cookies.txt")
-med.add_cookies(cookie_dict=cookie_dict)
-med.download_video(videoLink="https://mediaspace.illinois.edu/media/t/1_9cgs8jtvjk",videoName="374discussion0403")
